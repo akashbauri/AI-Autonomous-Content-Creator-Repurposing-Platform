73798ce29a2d13d58c2ec664dbd62c9eca1cd4d2
Akash bauri <108834431+akashbauri@users.noreply.github.com>
2025-12-17 02:36:18 +0530

    Update helper.py

--- helper.py
+++ helper.py
@@ -5,5 +5,5 @@ def load_env():
     if hasattr(st, "secrets"):
         os.environ["GROQ_API_KEY"] = st.secrets.get("GROQ_API_KEY", "")
         os.environ["SERPER_API_KEY"] = st.secrets.get("SERPER_API_KEY", "")
-        os.environ["VIDEO_API_KEY"] = st.secrets.get("VIDEO_API_KEY", "")
+        
 
