0cab00d83a328a65bb7b18591ed64fd6d2825cfd
Akash bauri <108834431+akashbauri@users.noreply.github.com>
2025-12-17 01:27:45 +0530

    Delete utils/utils directory

--- utils/utils/web_scraper.py
+++
@@ -1,8 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-def extract_website_content(url):
-    response = requests.get(url, timeout=10)
-    soup = BeautifulSoup(response.text, "html.parser")
-    paragraphs = [p.get_text() for p in soup.find_all("p")]
-    return " ".join(paragraphs[:50])
